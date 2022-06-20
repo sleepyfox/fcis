9e4afaf5873e2880386db9945037a6fdcd3500c5
Sleepy Fox <sleepyfox@gmail.com>
2022-06-20 11:22:35 +0100

    refactor: extract function cms_get_posts
    Extract everything to do with the responsibility of communicating with
    the CMS to a helper function that has the HTTP protocol adaptor
    injected in.

--- node/cms.js
+++ node/cms.js
@@ -3,37 +3,44 @@ const fs = require('fs')
 const axios = require('axios').default
 const CMS_URL = 'http://jsonplaceholder.typicode.com/posts'
 
+// Get posts, returns an array of post objects
+// Responsible for everything around the CMS DB, including:
+// transport rotocol (HTTP), location (URI) and payload schema (JSON)
+async function cms_get_posts(fetch) {
+  try {
+    var result = await fetch.get(CMS_URL)
+    console.log('getting CMS data')
+  } catch (err) {
+    console.log('an error occurred getting the post data', err)
+    return Promise.reject('error')
+  }
+  if (result.status === 200) {
+    return Promise.resolve(result.data)
+  } else {
+    console.log('error occurred, status =', result.status)
+    return Promise.reject('error')
+  }
+}
+
 module.exports = {
   main: async function(context, filesystem = fs, fetch = axios) {
-    try {
-      const res = await fetch.get(CMS_URL)
-      console.log('Getting CMS data')
-
-      if (res.status === 200) {
-        console.log('CMS data has: ', res.data.length, 'records')
-        let today = new Date()
-        today = today.toISOString().slice(0,10)
-        let summary = { posts: 0, users: 0, posts_per_user: 0 }
-        let users = []
-        for (let i = 0; i < res.data.length; i++) {
-          // console.log('processing record', i, 'for user', res.data[i].userId)
-          summary.posts++
-          if (!users.includes(res.data[i].userId)) {
-            users.push(res.data[i].userId)
-          }
-        }
-        summary.users = users.length
-        summary.mean_posts_per_user = Math.round(summary.posts / summary.users)
-        filesystem.writeFileSync('cms-' + today + '.json', JSON.stringify(summary))
-        console.log('Wrote CMS report')
-        return Promise.resolve(true)
-      } else {
-        console.log('Error occurred, status =', res.status)
-        return Promise.reject('error')
+    posts = await cms_get_posts(fetch)
+    console.log('CMS data has: ', posts.length, 'records')
+    let today = new Date()
+    today = today.toISOString().slice(0,10)
+    let summary = { posts: 0, users: 0, posts_per_user: 0 }
+    let users = []
+    for (let i = 0; i < posts.length; i++) {
+      // console.log('processing record', i, 'for user', res.data[i].userId)
+      summary.posts++
+      if (!users.includes(posts[i].userId)) {
+        users.push(posts[i].userId)
       }
-    } catch (err) {
-      console.log('An error occurred getting the post data', err)
-      return Promise.reject('error')
     }
+    summary.users = users.length
+    summary.mean_posts_per_user = Math.round(summary.posts / summary.users)
+    filesystem.writeFileSync('cms-' + today + '.json', JSON.stringify(summary))
+    console.log('wrote CMS report')
+    return Promise.resolve(true)
   }
 }
